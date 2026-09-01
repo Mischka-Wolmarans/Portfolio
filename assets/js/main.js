1000043b44b6048b0de0bd33857f19ed374600a7
Mischka-Wolmarans <mischka202019@gmail.com>
2026-09-01 09:48:44 +0200

    Updates 2026/09/01

--- assets/js/main.js
+++ assets/js/main.js
@@ -136,6 +136,25 @@
   }
 
   /**
+   * Right Place
+   */
+
+  const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses()
+    panel.classList.add('active')
+  })
+})
+
+function removeActiveClasses() {
+  panels.forEach(panel => {
+    panel.classList.remove('active')
+  })
+}
+
+  /**
    * Skills animation
    */
   let skilsContent = select('.skills-content');
